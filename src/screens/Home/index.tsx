863913d6ca2ae0f7208bc1c2c223154b921fa5c4
Luca Rodrigues <luca12rodrigues@gmail.com>
2021-10-23 15:57:43 -0300

    feat: create project base

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { View, KeyboardAvoidingView, Platform } from 'react-native'
+
+import { useAuth } from '../../hooks/auth'
+
+import { Header } from '../../components/Header'
+import { MessageList } from '../../components/MessageList'
+import { SigninBox } from '../../components/SigninBox'
+import { SendMessageForm } from '../../components/SendMessageForm'
+
+import { styles } from './styles'
+
+export function Home() {
+  const { user } = useAuth()
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <View style={styles.container}>
+        <Header />
+        <MessageList />
+        { user ? <SendMessageForm /> : <SigninBox /> }
+      </View>
+    </KeyboardAvoidingView>
+  )
+}
